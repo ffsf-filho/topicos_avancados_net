5640e328883be5205f26685c8a1eeacb10c02d79
FRANCISCO FLORENTINO S FILHO <franciscof@cetsp.com.br>
2024-10-19 16:41:48 -0300

    Implementa a aula sobre Lists e LinkedLists

--- alura-memoria-Aula-1/UsuarioLib/Usuario.cs
+++ alura-memoria-Aula-1/UsuarioLib/Usuario.cs
@@ -1,4 +1,6 @@
-﻿namespace UsuarioLib;
+﻿using System.Diagnostics;
+
+namespace UsuarioLib;
 
 public class Usuario
 {
@@ -8,12 +10,39 @@ public class Usuario
         Nome = nome;
         Email = email;
         Telefones = telefone;
+        ChavesDeAcessoList = new(new Guid[10]);
+        ChavesDeAcessoLinkedList = new();
+
+        Stopwatch stopwatchList = new();
+        stopwatchList.Start();
+
+        for (int i = 0; i < 1000000; i++)
+        {
+            ChavesDeAcessoList.Add(Guid.NewGuid());
+        }
+
+        stopwatchList.Stop();
+        Console.WriteLine($"Tempo total em ms: List {stopwatchList.Elapsed.TotalMilliseconds}");
+
+        Stopwatch stopwatchLikedList = new();
+        stopwatchLikedList.Start();
+        ChavesDeAcessoLinkedList.AddFirst(Guid.NewGuid());
+
+        for (int i = 0; i < 100000; i++) 
+        {
+            ChavesDeAcessoLinkedList.AddBefore(ChavesDeAcessoLinkedList.First!,Guid.NewGuid());
+        }
+        stopwatchLikedList.Stop();
+
+        Console.WriteLine($"Tempo total em ms: LikedList {stopwatchLikedList.Elapsed.TotalMilliseconds}");
     }
 
     public int Id { get; set; }
     public string Nome { get; set; }
     public string Email { get; set; }
     public List<string> Telefones { get; set; }
+    public List<Guid> ChavesDeAcessoList { get; set; }
+    public LinkedList<Guid> ChavesDeAcessoLinkedList { get; set; }
 
     public void PadronizaTelefones()
     {
